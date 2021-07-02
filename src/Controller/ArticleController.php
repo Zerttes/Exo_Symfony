1dde297fe9f3f072a55cde775b4b7dafaae539d4
lapiscine <lucas.duniau@lapiscine.pro>
2021-07-02 10:48:06 +0200

    id

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
@@ -15,4 +16,12 @@ class ArticleController
  {
      return new Response ('listes articles');
 }
+    /**
+     * @Route("/article/{id}", name="articleShow")
+     */
+    public function article ($id)
+    {
+        return new Response($id);
+    }
+
 }
